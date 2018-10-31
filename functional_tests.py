e64474c7092bed2359eaceb69c965a5f6a4259db
Samuel Barbosa <contato@smkbarbosa.eti.br>
2018-10-31 16:48:33 -0300

    update with first example in cap 2

--- functional_tests.py
+++ functional_tests.py
@@ -1,12 +1,47 @@
-#importa o selenium
 from selenium import webdriver
 
 
-#seleciona o Firefox como driver
 browser = webdriver.Firefox()
-#chama o endereço localhost:8000
+
+
+# Edith ouviu falar de uma nova aplicação online interessante para
+# lista de tarefas. Ela decide verificar sua homepage
 browser.get('http://localhost:8000')
 
 
-#testa se o titulo do  browser é django
-assert 'Django' in browser.title
+# Ela percebe que o título da página e o cabeçalho mencionam listas de
+# tarefas (to-do)
+assert 'To-Do' in browser.title
+
+
+#Ela é convidada a inserir um item de tarefa imediatamente
+
+
+# Ela digita "Buy peacock feathers" (Comprar penas de pavão) em uma caixa de
+# texto (o hobby de Edith é fazer iscas para pesca com fly)
+
+
+# Que ela tecla enter, a página é atualizada, e agora a página lista
+# '1: Buy peacock feathers" como um item em uma lista de tarefas
+
+
+# Ainda continua havendo uma caixa de texto convidando-a a acrescentar outro
+# item. Ela insere "Use pacock feathers to make a fly" (Usar penas de pavão
+# para fazer um fly - Edith é bem metódica)
+
+
+# A página é atualizada novamente e agora mostra os dois itens em sua lista
+
+
+# Edith se pergunta se o site lembrará de sua lista. Então ela nota
+# que o site gerou um URL único para ela -- há um pequeno
+# texto explicativo para isso.
+
+
+# Ela acessa esse URL - sua lista de tarefas continua lá.
+
+
+# Satisfeita, ela volta a dormir
+
+
+browser.quit()
