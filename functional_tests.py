c3e86ff6574424c1e3c3008deda2f8f6d0158e61
Samuel Barbosa <contato@smkbarbosa.eti.br>
2018-11-28 15:26:29 -0300

    Alter Functional_Tests

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# Edith ouviu falar de uma nova aplicação online interessante para
 		# lista de tarefas. Ela decide verificar sua homepage
@@ -35,22 +40,24 @@ class NewVisitorTest(unittest.TestCase):
 		# texto (o hobby de Edith é fazer iscas para pesca com fly)
 		inputbox.send_keys('Buy peacock feathers')
 
-		# Que ela tecla enter, a página é atualizada, e agora a página lista
+		# Quando ela tecla enter, a página é atualizada, e agora a página lista
 		# '1: Buy peacock feathers" como um item em uma lista de tarefas
 		inputbox.send_keys(Keys.ENTER)
-		time.sleep(10)
-
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-		self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+		time.sleep(1)
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 		# Ainda continua havendo uma caixa de texto convidando-a a acrescentar outro
 		# item. Ela insere "Use pacock feathers to make a fly" (Usar penas de pavão
 		# para fazer um fly - Edith é bem metódica)
-		self.fail('Finish the test!')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
 
+	
 		# A página é atualizada novamente e agora mostra os dois itens em sua lista
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
 		# Edith se pergunta se o site lembrará de sua lista. Então ela nota
 		# que o site gerou um URL único para ela -- há um pequeno
